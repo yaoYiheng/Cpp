40de4081d555e803cd00a6a62c23a5aae1385edb
yaoYiheng <fn1117@qq.com>
2018-08-19 23:07:54 +0800

    helloworld

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by 姚懿恒 on 2018/8/19.
+//  Copyright © 2018年 姚懿恒. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
