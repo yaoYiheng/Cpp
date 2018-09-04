39e1ca51600392df28ffb0d7d6a8bf7d7abedb47
yaoYiheng <fn1117@qq.com>
2018-09-04 16:07:18 +0800

    vprt指针的初始化
    在调用子类的构造函数之前, 会调用父类的构造函数, 当还在父类的构造函数内时, vptr指向的还是父类的虚函数列表, 只有当程序来到子类构造函数时, vptr才会指向子类的虚函数列表.

--- Day7/Day7/main.cpp
+++ Day7/Day7/main.cpp
@@ -36,10 +36,14 @@ public:
     Parent()
     {
         this->a = 0;
+        func();
     }
     Parent(int a)
     {
+        cout << "父类Parent(int a)被调用" << endl;
         this->a = a;
+        
+        func();
     }
     //
     virtual void testInChild()
@@ -65,14 +69,15 @@ class Child : public Parent
     
 public:
     
-    Child()
+    Child():Parent()
     {
-        this->a = 0;
+        
         this->b = 0;
     }
-    Child(int a, int b)
+    Child(int a, int b):Parent(a)
     {
-        this->a = a;
+        func();
+        cout << "子类Child(int a, int b):Parent(a)被调用" << endl;
         this->b = b;
     }
     int a;
@@ -84,7 +89,7 @@ public:
     }
     void testInChild()
     {
-        cout << "父类是否能调用" << endl;
+        cout << "这是在子类中调用" << endl;
     }
 };
 
@@ -92,25 +97,31 @@ void myFunc(Parent * pp)
 {
     pp->func();
 }
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    
+
+void TestOne()
+{
     A a;
     B b;
-
+    
     cout << "sizeA = " << sizeof(a) << endl;
     cout << "sizeb = " << sizeof(b) << endl;
     
     
     Parent *pp = new Child(12, 23);
-     cout << "sizePP = " << sizeof(pp) << endl;
+    cout << "sizePP = " << sizeof(pp) << endl;
     
     Child *cp = new Child();
     pp->func();//如果调用的是一个不再虚函数列表内的普通函数, 那么编译器根本不会去查找虚函数列表, 只有当调用的是虚函数时, 才会去查找虚函数列表
     
     pp->testInChild();// 指向子类对象的父类指针, 不能够调用子类中, 不存在与父类的函数.
-                    //如果想通过指向子类对象的父类指针调用在子类中与父类完全相同的函数, 则需要在父类中, 给那个函数添加virtual
+    //如果想通过指向子类对象的父类指针调用在子类中与父类完全相同的函数, 则需要在父类中, 给那个函数添加virtual
+}
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    Parent *cp = new Child(12, 23);
 
 
+    delete cp;
     return 0;
 }
