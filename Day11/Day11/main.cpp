e8b2fad806b3d03dc8e40b66aa473f2b94e60a34
yaoYiheng <fn1117@qq.com>
2018-10-17 16:28:29 +0800

    deque 赋值assin, 交换, 元素个数

--- Day11/Day11/main.cpp
+++ Day11/Day11/main.cpp
@@ -35,11 +35,39 @@ void practise1()
     printDeque(d4);
 }
 
+
+/**
+ deque赋值 大小操作
+ */
+void practise2()
+{
+    deque<int> d1(10, 4);
+    deque<int > d2;
+    
+    d2.assign(d1.begin(), d1.end());//将[begin, end)区间中的数据拷贝赋值给本身
+    d2.assign(4, 10);//将n个elem拷贝赋值给本身
+    d2 = d1;//重载等号操作符
+    deque<int>(3, 3).swap(d2);//将元素互换
+    
+    printDeque(d2);
+    
+    cout << "deque中的元素个数为:" << d2.size() << endl;
+    
+    d2.empty()? cout << "为空" << endl: cout << "不为空" <<endl;
+    
+    d2.resize(6, 100); //重新制定容器长度为num, 如果变长, 则以elem值填充新位置, 如果变短,则删除超出长度元素
+    
+    printDeque(d2);
+    
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     
     
-    practise1();
+//    practise1();
+    
+    practise2();
     
     return 0;
 }
