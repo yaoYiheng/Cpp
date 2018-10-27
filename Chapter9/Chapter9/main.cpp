91acca33f2b4618b382e8ea644f2eff2464e9f2e
yaoYiheng <fn1117@qq.com>
2018-10-27 14:44:10 +0800

    第九章

---
+++ Chapter9/Chapter9/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Chapter9
+//
+//  Created by 姚懿恒 on 2018/10/27.
+//  Copyright © 2018年 姚懿恒. All rights reserved.
+//
+
+#include <iostream>
+#include <array>
+#include <list>
+#include <deque>
+
+using namespace std;
+
+
+/**
+ 定义g一个list对象, 其元素类型是int的deque
+ */
+void practise9_1()
+{
+    list<deque<int>> list1;
+}
+
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
