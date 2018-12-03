21bb5ce761ae0c285257f8a0297e8daa38eda57d
yaoYiheng <fn1117@qq.com>
2018-12-04 00:25:52 +0800

    开闭原则Init

---
+++ Day14/Day14/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Day14
+//
+//  Created by 姚懿恒 on 2018/12/4.
+//  Copyright © 2018年 姚懿恒. All rights reserved.
+//
+
+#include <iostream>
+
+/*
+ 开闭原则
+ 对扩展开放, 对修改关闭, 增加功能是通过增加代码来实现,而不是通过
+ 修改源代码来实现
+ */
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
